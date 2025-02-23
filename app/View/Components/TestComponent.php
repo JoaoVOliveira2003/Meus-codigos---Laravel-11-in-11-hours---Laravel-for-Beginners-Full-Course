b05bc41ddd670b8c99180f9c2f5760726bfb32d6
JoaoVOliveira2003 <ojoao953@gmail.com>
2025-02-23 11:01:38 -0300

    .

--- app/View/Components/TestComponent.php
+++ app/View/Components/TestComponent.php
@@ -6,18 +6,25 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class SearchForm extends Component
+class TestComponent extends Component
 {
     /**
      * Create a new component instance.
      */
-    public function __construct(public string $action = "/search",public string $method="GET")
+    public function __construct()
     {
-        
+        //
     }
 
+    /**
+     * Get the view / contents that represent the component.
+     */
     public function render(): View|Closure|string
     {
-        return view('components.search-form');
+        return <<<'blade'
+<div {{$attributes}}>
+    {{$slot}}
+</div>
+blade;
     }
 }
--- resources/views/components/admin/card.blade.php
+++ resources/views/components/admin/card.blade.php
@@ -1,3 +1,20 @@
-<div>
-    Opas
+@props(['color', 'bgColor' => 'white', 'title' => '', 'footer' => ''])
+
+<div {{ $attributes->merge(['class' => "card card-text-$color card-bg-$bgColor"]) }}>
+    
+    @if (!empty($title))
+        <div class="card-header">
+            {{ $title }}
+        </div>
+    @endif
+
+    <div class="card-body">
+        {{ $slot ?: 'Insira um valor' }}
+    </div>
+
+    @if (!empty($footer))
+        <div class="card-footer">
+            {{ $footer }}
+        </div>
+    @endif
 </div>
